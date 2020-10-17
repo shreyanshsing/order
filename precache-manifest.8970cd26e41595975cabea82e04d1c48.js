59dccb5de8eb4babd6c7be4a454e6f8691c5832b
shreyanshsingh <shreyanshsinghjee@gmail.com>
2020-10-17 19:17:07 +0530

    Updates

---
+++ precache-manifest.8970cd26e41595975cabea82e04d1c48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d4d717704da7c2845f4e5c007aa0f3",
+    "url": "/order/index.html"
+  },
+  {
+    "revision": "0e06b63a3f2d0bdb2493",
+    "url": "/order/static/css/main.6ee9c57c.chunk.css"
+  },
+  {
+    "revision": "c0ca2ba0d6c9dfa8f57e",
+    "url": "/order/static/js/2.9f1c2bf3.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/order/static/js/2.9f1c2bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e06b63a3f2d0bdb2493",
+    "url": "/order/static/js/main.60c90754.chunk.js"
+  },
+  {
+    "revision": "f27096b989670fede9b0",
+    "url": "/order/static/js/runtime-main.cc33ce03.js"
+  }
+]);
